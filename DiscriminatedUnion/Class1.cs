dc71e732528df3e1035541572dbe4869fe6b6b45
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-11-07 20:53:17 +0530

    Initial commit

---
+++ DiscriminatedUnion/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DiscriminatedUnionLib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ DiscriminatedUnionGenerator/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DiscriminatedUnionGenerator
+{
+    public class Class1
+    {
+
+    }
+}
