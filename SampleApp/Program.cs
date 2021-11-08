8d2864dc648b72e2e1715f9845234e7bf3a210aa
Shivendra Pratap Singh <shivendrapsingh014@gmail.com>
2021-11-08 15:57:07 +0530

    nested expression

--- SampleApp/Program.cs
+++ SampleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DiscriminatedUnion;
+using Test;
 
 UType u=new UType.Circle(6);
 
@@ -6,7 +7,7 @@ if( u is UType.Circle c)
     Console.WriteLine(c.Radius);
 
 else if(u is UType.Rectangle r)
-    Console.WriteLine(r.Width*r.Height);
+    Console.WriteLine(r.Width+" "+r.Height);
 
 u = new UType.Rectangle(12, 10);
 //or 
@@ -19,6 +20,10 @@ switch(u.Is)
         Console.WriteLine(u.AsRectangle().Height);
         break;
 }
+
+Enumz sym = new Enumz.Circle(10);
+sym = new Enumz.Special(sym);
+Console.WriteLine(sym.AsSpecial().Left.Is);
 enum Type
 {
     [UnionType<int>("Radius")]
@@ -39,6 +44,10 @@ namespace Test
 
         [UnionType<double>("Width")]
         [UnionType<double>("Height")]
-        Rectangle
+        Rectangle,
+
+        [UnionType<Enumz>("Left")]
+        Special
+
     }
 }
